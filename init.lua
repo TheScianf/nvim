1340ae87ed08a2d60f821a5fc5c1e592c212bc01
Vincenz8 <vincenzo.russ8@outlook.com>
2025-02-06 23:54:01 +0100

    java formatter

--- init.lua
+++ init.lua
@@ -15,7 +15,6 @@ vim.g.have_nerd_font = true
 
 -- Make line numbers default
 vim.opt.relativenumber = true
-
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
 
@@ -511,14 +510,6 @@ require('lazy').setup({
       local servers = {
         pylsp = {},
         jdtls = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
@@ -608,8 +599,7 @@ require('lazy').setup({
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         python = { 'ruff' },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
+        java = { 'google-java-format' },
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
       },
     },
