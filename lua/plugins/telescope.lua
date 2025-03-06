4c4c44f60f7275b65462758116d5d5645e7e6a77
Vincenz8 <vincenzo.russ8@outlook.com>
2025-03-06 09:39:16 +0100

    update

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -75,6 +75,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+    vim.keymap.set('n', '<leader>sc', builtin.colorscheme, { desc = '[S]earch colorscheme' })
     vim.api.nvim_set_keymap('n', '<leader><leader>', ':Telescope cmdline<CR>', { noremap = true, desc = 'Cmdline' })
     -- Slightly advanced example of overriding default behavior and theme
     vim.keymap.set('n', '<leader>/', function()
