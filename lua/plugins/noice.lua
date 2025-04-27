02e3371d48945661e3376dbae954e9ab041e4675
Vincenz8 <vincenzo.russ8@outlook.com>
2025-04-27 20:31:56 +0200

    deleted noice shortcut

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -11,7 +11,4 @@ return -- lazy.nvim
       command_palette = true, -- position the cmdline and popupmenu together
     }, -- add any options here
   },
-  config = function()
-    vim.keymap.set('n', '<leader>nd', '<cmd>NoidceDismiss<CR>', { desc = 'Dismiss noice notification' })
-  end,
 }
