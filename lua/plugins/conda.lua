4f157827a7011334bd357cb7c626066b89f48082
Vincenz8 <vincenzo.russ8@outlook.com>
2025-07-26 14:43:47 +0200

    removed plugins

--- lua/plugins/conda.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'kmontocam/nvim-conda',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    vim.keymap.set('n', '<leader>ce', '<cmd>CondaActivate<CR>', { desc = 'Activate conda environment' })
-    vim.keymap.set('n', '<leader>cd', '<cmd>CondaDeactivate<CR>', { desc = 'Deactivate conda environment' })
-  end,
-}
