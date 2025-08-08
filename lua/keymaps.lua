d29c3c8db6a47271047dcaaa4cbb74f845052718
Vincenz8 <vincenzo.russ8@outlook.com>
2025-08-08 12:31:18 +0200

    refactoring and new keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,5 +1,8 @@
 vim.g.mapleader = ' '
+
 vim.g.maplocalleader = ' '
+
+vim.cmd.colorscheme('unokai')
 
 vim.g.have_nerd_font = true
 
@@ -36,8 +39,8 @@ vim.opt.smartcase = true
 -- Keep signcolumn on by default
 vim.opt.signcolumn = 'yes'
 
--- Decrease update time
-vim.opt.updatetime = 250
+-- Hide cmdline after entering command
+vim.opt.cmdheight = 0
 
 -- Decrease mapped sequence wait time
 vim.opt.timeoutlen = 300
@@ -56,19 +59,19 @@ vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 vim.opt.inccommand = 'split'
 
 -- Show which line your cursor is on
-vim.opt.cursorline = false
+vim.opt.cursorline = true
 
 vim.opt.statusline = "%f %m%r%=%y %l/%L"
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
---  See `:help vim.keymap.set()`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+-- Highlight option while searching
+vim.opt.hlsearch = false
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- for people to discover. Otherwise, fou normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
@@ -77,6 +80,7 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 
 -- TIP: Disable arrow keys in normal mode
 vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+
 vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
@@ -90,18 +94,21 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
--- Terminal in new tab keymap
-vim.api.nvim_set_keymap('n', '<leader>tt', ':tabnew | terminal<CR>',
-  { desc = 'Open a new terminal in a different tab', noremap = true, silent = true })
+-- Tab keymaps
+vim.keymap.set('n', '<leader>t', ':tabnew',
+  { desc = 'Open new tab', noremap = true, silent = true })
+vim.keymap.set('n', '<leader>tc', ':tabclose<CR>', { desc = 'Close current tab', noremap = true, silent = true })
 
--- Closing tab
-vim.api.nvim_set_keymap('n', '<leader>tc', ':tabclose<CR>', { desc = 'Close current tab', noremap = true, silent = true })
+-- New terminal
+vim.keymap.set('n', '<leader>te', ':sp | te<CR>',
+  { desc = 'Open new terminal in the same tab', noremap = true, silent = true })
 -- Visual all buffer
 vim.keymap.set('n', '<leader>va', 'ggVG', { desc = 'Visual select all buffer', noremap = true, silent = true })
 
 -- center to screen when jumping to line
 vim.keymap.set('n', 'j', 'jzz', { desc = 'Move down and center', noremap = true, silent = true })
 vim.keymap.set('n', 'k', 'kzz', { desc = 'Move up and center', noremap = true, silent = true })
+vim.keymap.set('n', 'G', 'Gzz', { desc = 'Move to end of file and center', noremap = true, silent = true })
 
 -- Autopair brackets and quotes with cursor positioning
 vim.keymap.set('i', '(', '()<Left>', { desc = 'Autopair (', noremap = true, silent = true })
@@ -111,3 +118,8 @@ vim.keymap.set('i', '"', '""<Left>', { desc = 'Autopair "', noremap = true, sile
 vim.keymap.set('i', "'", "''<Left>", { desc = "Autopair '", noremap = true, silent = true })
 vim.keymap.set('i', '`', '``<Left>', { desc = 'Autopair `', noremap = true, silent = true })
 vim.keymap.set('i', '<', '<><Left>', { desc = 'Autopair <', noremap = true, silent = true })
+
+-- Change colorscheme from light to dark and viceversa
+vim.keymap.set('n', '<leader>bg', function()
+  vim.cmd.colorscheme(vim.g.colors_name == "unokai" and "shine" or "unokai")
+end, { desc = 'Toggle between unokai and shine colorschemes' })
