ba3ea1568f75739b1760add8d87102ac1fb472c9
Vincenz8 <vincenzo.russ8@outlook.com>
2025-08-14 11:16:34 +0200

    completion suggestion order

--- lua/plugins/codeflow/autocompletion.lua
+++ lua/plugins/codeflow/autocompletion.lua
@@ -1,3 +1,9 @@
+local source_priority = {
+  snippets = 4,
+  lsp = 3,
+  path = 2,
+  buffer = 1,
+}
 return {
   'saghen/blink.cmp',
   dependencies = { 'L3MON4D3/LuaSnip', version = 'v2.*' },
@@ -20,10 +26,24 @@ return {
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      default = { 'snippets', 'lsp', 'path', 'buffer' },
     },
 
-    fuzzy = { implementation = 'prefer_rust_with_warning' },
+    fuzzy = {
+      implementation = 'prefer_rust_with_warning',
+      sorts = {
+        function(a, b)
+          local a_priority = source_priority[a.source_id]
+          local b_priority = source_priority[b.source_id]
+          if a_priority ~= b_priority then
+            return a_priority > b_priority
+          end
+        end,
+        -- defaults
+        'score',
+        'sort_text',
+      },
+    },
   },
   opts_extend = { 'sources.default' },
 }
