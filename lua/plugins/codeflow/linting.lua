a639f48206bb6d0d8a06c367cd9dc2575319b569
Vincenz8 <vincenzo.russ8@outlook.com>
2026-02-20 00:06:14 +0100

    fixed markdown error

--- lua/plugins/codeflow/linting.lua
+++ lua/plugins/codeflow/linting.lua
@@ -1,9 +1,18 @@
 return {
   'mfussenegger/nvim-lint',
-  opts = {
-    linters_by_ft = { lua = { 'luacheck' }, python = { 'ruff' }, java = { 'checkstyle' }, c = { 'cpplint' }, kotlin = { 'ktlint' }, json = { 'jsonlint' } },
-  },
   config = function()
+    -- 1. Manually assign the linters here
+    require('lint').linters_by_ft = {
+      lua = { 'luacheck' },
+      python = { 'ruff' },
+      java = { 'checkstyle' },
+      c = { 'cpplint' },
+      kotlin = { 'ktlint' },
+      json = { 'jsonlint' },
+      markdown = {}, -- This will now actually work
+    }
+
+    -- 2. Create the autocommand
     vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
       callback = function()
         require('lint').try_lint()
